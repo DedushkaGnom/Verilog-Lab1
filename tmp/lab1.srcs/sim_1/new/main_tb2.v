2778cee417f4bffa136482940800f7df030e2ff0
Andrey <dedushkagnom@yandex.ru>
2020-04-07 20:38:17 +0300

    first commit

---
+++ tmp/lab1.srcs/sim_1/new/main_tb2.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.03.2020 21:38:51
+// Design Name: 
+// Module Name: main_tb2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main_tb2(
+    input [15:0] sw,
+    output [15:0] l
+    
+    always @(*) begin
+     
+    end
+    
+    );
+endmodule
